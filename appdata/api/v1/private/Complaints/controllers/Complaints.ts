6dd53d6be9a8e6ceec728881b2efd60e9a826f7f
Shajee98 <s.bin.17878@khi.iba.edu.pk>
2024-02-21 04:54:23 +0500

    attachment update done

--- appdata/api/v1/private/Complaints/controllers/Complaints.ts
+++ appdata/api/v1/private/Complaints/controllers/Complaints.ts
@@ -1,158 +1,250 @@
-import complaintService from "../../../../../../services/complaints/Complaints"
-import responses from "../../../../../../constants/Responses"
+import complaintService from "../../../../../../services/complaints/Complaints";
+import responses from "../../../../../../constants/Responses";
 import { RequestHandler } from "express";
-import { serverErrorResponse, successResponse } from "../../../../../../services/Response/Response";
+import {
+  serverErrorResponse,
+  successResponse,
+} from "../../../../../../services/Response/Response";
 import Attachment from "../../../../../../models/Attahcment";
-import fs from 'fs'
+import fs from "fs";
 import path from "path";
 
 export const createComplaint: RequestHandler = async (req, res, next) => {
   try {
     console.log("req...", req.body);
-    const { customer_number, complaint_number, description, staff_id, department_id, complaint_status_id, complaint_type_id, fromWhatsapp, customer_name, created_by } = req.body
-    console.log("customer_number ", customer_number, "complaint_number ", complaint_number, "description ", description, staff_id, "department_id ", department_id, "complaint_status_id", complaint_status_id)
-    const complaint = await complaintService.createComplaint({customer_number, complaint_number, description, staff_id, department_id, complaint_status_id, complaint_type_id, fromWhatsapp, customer_name, created_by})
+    const {
+      customer_number,
+      complaint_number,
+      description,
+      staff_id,
+      department_id,
+      complaint_status_id,
+      complaint_type_id,
+      fromWhatsapp,
+      customer_name,
+      attachments,
+      created_by,
+    } = req.body;
+    console.log(
+      "customer_number ",
+      customer_number,
+      "complaint_number ",
+      complaint_number,
+      "description ",
+      description,
+      staff_id,
+      "department_id ",
+      department_id,
+      "complaint_status_id",
+      complaint_status_id,
+      "attachments",
+      attachments
+    );
+    const complaint = await complaintService.createComplaint({
+      customer_number,
+      complaint_number,
+      description,
+      staff_id,
+      department_id,
+      complaint_status_id,
+      complaint_type_id,
+      fromWhatsapp,
+      customer_name,
+      created_by,
+    });
     if (!complaint) {
       return serverErrorResponse(res, responses.ORDER_NOT_CREATED);
     }
 
-    if (req.files)
-    {
-      let files: any = req.files
-      await Promise.all(files.map(async (file: any) => {
-        console.log("file ====> ", file)
-        await Attachment.create({
-          fileName: file.originalname,
-          complaintId: complaint.id
+    if (req.files) {
+      let files: any = req.files;
+      await Promise.all(
+        files.map(async (file: any) => {
+          console.log("file ====> ", file);
+          await Attachment.create({
+            fileName: file.originalname,
+            complaintId: complaint.id,
+          });
         })
-      }));
+      );
     }
 
-    return successResponse(res, {complaint});
+    return successResponse(res, { complaint });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const updateComplaint: RequestHandler = async (req, res, next) => {
   try {
-    const { id } = req.params
-    const complaint = await complaintService.updateComplaint(Number(id), {...req.body})
+    const { id } = req.params;
+    console.log("body => ", req.body);
+    const complaint = await complaintService.updateComplaint(Number(id), {
+      ...req.body,
+    });
+    console.log("complaint updated => ", complaint[1][0]);
     if (!complaint || complaint[0] == 0) {
       return serverErrorResponse(res, responses.NOT_UPDATED);
     }
-      // const oldAttachments = await Attachment.findAll({
-      //   where: {
-      //     complaint_id: complaint[1][0].dataValues.id
-      //   }
-      // })
-          // await Promise.all(oldAttachments.map(async (attachment) => {
-          //   await Attachment.destroy({
-          //     where: {
-          //       id: attachment.dataValues.id
-          //     }
-          //   })
-            // fs.unlink('/path/to/file.txt', (err) => {
-            //   if (err) {
-            //     console.error(err);
-            //     return;
-            //   }
-            //   console.log('File deleted successfully');
-            // });
-          // }))
+    const oldAttachments = await Attachment.findAll({
+      where: {
+        complaint_id: id,
+      },
+    });
+    console.log("oldAttachments => ", oldAttachments);
 
-      // if (oldAttachments.length > files.length)
-      // {
-      //   for (let i = 0; i < files.length; i++) {
-      //     let removeAttachments = oldAttachments.filter((attachment) => attachment.dataValues.file_name != files[i].originalname)
-      //     await Promise.all(removeAttachments.map(async (attachment) => {
-      //       await Attachment.destroy({
-      //         where: {
-      //           id: attachment.dataValues.id
-      //         }
-      //       })
-      //     }))
-      //   }
-      // }
-      // else if (files.length > oldAttachments.length)
-      // {
-      //   for (let i = 0; i < oldAttachments.length; i++) {
-      //     let addAttachments = files.filter((file : any) => file.originalname != oldAttachments[i].dataValues.file_name)
-      //     await Promise.all(addAttachments.map(async (attachment: any) => {
-      //       await Attachment.create({
-      //         fileName: attachment.originalname,
-      //         complaintId: complaint[1][0].dataValues.id
-      //       })
-      //     }))
-      //   }
-      // }
-      if (req.files)
-      {
-        let files: any = req.files
-        await Promise.all(files.map(async (file: any) => {
-          console.log("file ====> ", file)
+    let removeOldAttachments: any[] = [];
+    let unremovedOldAttachments: any[] = [];
+    if (JSON.parse(req.body?.unremovedOldAttachments).length != 0) {
+      unremovedOldAttachments = JSON.parse(req.body?.unremovedOldAttachments);
+      console.log("unremovedOldAttachments => ", unremovedOldAttachments);
+      removeOldAttachments = oldAttachments.filter(item => !unremovedOldAttachments.includes(item.dataValues.fileName));
+      console.log("removeOldAttachments => ", removeOldAttachments);
+      if (removeOldAttachments.length != 0) {
+        await Promise.all(
+          removeOldAttachments.map(async (attachment) => {
+            await Attachment.destroy({
+              where: {
+                id: attachment.dataValues.id,
+              },
+            });
+            fs.unlink(`uploads/attachments/${attachment.fileName}`, (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              console.log("File deleted successfully");
+            });
+          })
+        );
+      } else {
+        console.log("no files changed");
+      }
+    } else {
+      await Promise.all(
+        oldAttachments.map(async (attachment) => {
+          await Attachment.destroy({
+            where: {
+              id: attachment.dataValues.id,
+            },
+          });
+          fs.unlink(
+            `uploads/attachments/${attachment.dataValues.fileName}`,
+            (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              console.log("File deleted successfully");
+            }
+          );
+        })
+      );
+    }
+
+    // if (oldAttachments.length > files.length)
+    // {
+    //   for (let i = 0; i < files.length; i++) {
+    //     let removeAttachments = oldAttachments.filter((attachment) => attachment.dataValues.file_name != files[i].originalname)
+    //     await Promise.all(removeAttachments.map(async (attachment) => {
+    //       await Attachment.destroy({
+    //         where: {
+    //           id: attachment.dataValues.id
+    //         }
+    //       })
+    //     }))
+    //   }
+    // }
+    // else if (files.length > oldAttachments.length)
+    // {
+    //   for (let i = 0; i < oldAttachments.length; i++) {
+    //     let addAttachments = files.filter((file : any) => file.originalname != oldAttachments[i].dataValues.file_name)
+    //     await Promise.all(addAttachments.map(async (attachment: any) => {
+    //       await Attachment.create({
+    //         fileName: attachment.originalname,
+    //         complaintId: complaint[1][0].dataValues.id
+    //       })
+    //     }))
+    //   }
+    // }
+    if (req.files) {
+      let files: any = req.files;
+      await Promise.all(
+        files.map(async (file: any) => {
+          console.log("file ====> ", file);
           await Attachment.create({
             fileName: file.originalname,
-            complaintId: complaint[1][0].dataValues.id
-          })
-        }));
-      }
-      return successResponse(res, {complaint});  
+            complaintId: Number(id),
+          });
+        })
+      );
+    }
+    return successResponse(res, { complaint });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const getAllComplaints: RequestHandler = async (req, res, next) => {
   try {
-    const { department_id, complaint_type_id, complaint_status_id } = req.query
-    console.log("department_id ==> ", department_id, " complaint_type_id ===> ", complaint_type_id, " complaint_status_id ===> ", complaint_status_id)
-    const user = req.user
-    const complaints = await complaintService.getAllComplaints(Number(department_id), Number(complaint_type_id), Number(complaint_status_id), user)
-    console.log("complaints ==> ", complaints)
+    const { department_id, complaint_type_id, complaint_status_id } = req.query;
+    console.log(
+      "department_id ==> ",
+      department_id,
+      " complaint_type_id ===> ",
+      complaint_type_id,
+      " complaint_status_id ===> ",
+      complaint_status_id
+    );
+    const user = req.user;
+    const complaints = await complaintService.getAllComplaints(
+      Number(department_id),
+      Number(complaint_type_id),
+      Number(complaint_status_id),
+      user
+    );
     if (!complaints) {
       return serverErrorResponse(res, responses.ORDER_CREATED);
+    } else if (complaints.count == 0) {
+      return successResponse(res, { complaints });
     }
-    else if (complaints.count == 0)
-    {
-      return successResponse(res, {complaints});
-    }
-    return successResponse(res, {complaints});
-
+    return successResponse(res, { complaints });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const getAllSearchByKeyword: RequestHandler = async (req, res, next) => {
   try {
     const { keyword } = req.query;
-    const complaints = await complaintService.getAllSearchByKeyword(String(keyword));
+    const complaints = await complaintService.getAllSearchByKeyword(
+      String(keyword)
+    );
     const responseData = complaints;
     successResponse(res, responseData);
     return;
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const getAllComplaintStatus: RequestHandler = async (req, res, next) => {
   try {
-    const statuses = await complaintService.getAllComplaintStatus()
+    const statuses = await complaintService.getAllComplaintStatus();
     if (!statuses) {
       return serverErrorResponse(res, responses.ORDER_CREATED);
     }
 
-    return successResponse(res, {statuses: statuses});
+    return successResponse(res, { statuses: statuses });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const getComplaintById: RequestHandler = async (req, res, next) => {
- try {
-    const { id } = req.params
-    const complaint = await complaintService.getComplaintById(Number(id))
+  try {
+    const { id } = req.params;
+    const complaint = await complaintService.getComplaintById(Number(id));
     if (!complaint) {
       return serverErrorResponse(res, responses.ORDER_NOT_FOUND);
     }
@@ -164,7 +256,7 @@ export const getComplaintById: RequestHandler = async (req, res, next) => {
     //       console.error(err);
     //       return
     //     }
-    
+
     //     // Get information about each file
     //     const filePromises = files.map(file => {
     //       const filePath = path.join("uploads\\attachments\\", attachment.fileName);
@@ -195,24 +287,27 @@ export const getComplaintById: RequestHandler = async (req, res, next) => {
     //       // matchingFiles.map((file: any) => {
     //         //     archive.file(file, { name: file });
     //         //   });
-            
+
     //         //   archive.finalize();
     //       })
     //     })
     //   })
     //   console.log("matchingFiles ====> ", matchingFiles)
-      return successResponse(res, {complaint});
-
+    return successResponse(res, { complaint });
   } catch (error) {
-    next(error)
- }
+    next(error);
+  }
 };
 
 const getComplaintsByMonth: RequestHandler = async (req, res, next) => {
   try {
     const { status_id, complaint_type_id } = req.query;
-    const id = req.user?.id
-    const complaints = await complaintService.getComplaintsByMonths(Number(id), Number(status_id), Number(complaint_type_id));
+    const id = req.user?.id;
+    const complaints = await complaintService.getComplaintsByMonths(
+      Number(id),
+      Number(status_id),
+      Number(complaint_type_id)
+    );
     const responseData = { ...complaints };
     successResponse(res, responseData);
     return;
@@ -223,43 +318,47 @@ const getComplaintsByMonth: RequestHandler = async (req, res, next) => {
 
 export const getComments: RequestHandler = async (req, res, next) => {
   try {
-    const { complaint_id } = req.query
-    const comments = await complaintService.getComments(Number(complaint_id))
-     if (!comments) {
-       return serverErrorResponse(res, responses.ORDER_NOT_FOUND);
-     }
- 
-     return successResponse(res, {comments});
+    const { complaint_id } = req.query;
+    const comments = await complaintService.getComments(Number(complaint_id));
+    if (!comments) {
+      return serverErrorResponse(res, responses.ORDER_NOT_FOUND);
+    }
+
+    return successResponse(res, { comments });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const addCommentToComplaint: RequestHandler = async (req, res, next) => {
   try {
-    const id = req.user?.id
-     const { complaint_id, comment } = req.body
-     const complaint = await complaintService.addCommentToComplaint(Number(complaint_id), Number(id), comment)
-     if (!complaint) {
-       return serverErrorResponse(res, responses.ORDER_NOT_FOUND);
-     }
- 
-     return successResponse(res, {complaint});
-   } catch (error) {
-     next(error)
+    const id = req.user?.id;
+    const { complaint_id, comment } = req.body;
+    const complaint = await complaintService.addCommentToComplaint(
+      Number(complaint_id),
+      Number(id),
+      comment
+    );
+    if (!complaint) {
+      return serverErrorResponse(res, responses.ORDER_NOT_FOUND);
+    }
+
+    return successResponse(res, { complaint });
+  } catch (error) {
+    next(error);
   }
- };
+};
 
 const complaintController = {
-    createComplaint,
-    updateComplaint,
-    getAllComplaints,
-    getComplaintById,
-    getComments,
-    getAllComplaintStatus,
-    getComplaintsByMonth,
-    addCommentToComplaint,
-    getAllSearchByKeyword
-}
+  createComplaint,
+  updateComplaint,
+  getAllComplaints,
+  getComplaintById,
+  getComments,
+  getAllComplaintStatus,
+  getComplaintsByMonth,
+  addCommentToComplaint,
+  getAllSearchByKeyword,
+};
 
-export default complaintController
+export default complaintController;
